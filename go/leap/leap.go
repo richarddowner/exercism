7b9cb1c2a64248d98b6d5feba8855860297aa683
Richard Downer <richard.downer@xero.com>
2014-01-24 08:49:09 +1300

    leap: ready to upload

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,19 +3,6 @@ package leap
 // If year is divisible by 400 then leap year
 // else if year is divisible by 100 then not leap year
 // else if year is divisible by 4 then leap year
-
-// if divisible by 4 but also divisible by 100 and not divisble by 400
 func IsLeapYear(year int) bool {
-	/*
-		if year%400 == 0 {
-			return true
-		} else if year%100 == 0 {
-			return false
-		} else if year%4 == 0 {
-			return true
-		} else {
-			return false
-		}
-	*/
-	return year%400 == 0 && year%100 != 0 || year%4 == 0
+	return (year%4 == 0 && year%100 != 0) || (year%4 == 0 && year%100 == 0 && year%400 == 0)
 }
