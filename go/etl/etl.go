e9333bea10c9b21540d80b5473d7884e34871512
Richard Downer <richard.downer@xero.com>
2014-01-24 10:46:18 +1300

    Correct way of passing in a map of key: int, value: array string

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -3,6 +3,6 @@ package etl
 // given map[int]array (old format)
 // return a map[string]int (new format)
 
-func Transform(map[int]string{}) map[string]int {
-
+func Transform(map[int][]string) map[string]int {
+	return nil
 }
