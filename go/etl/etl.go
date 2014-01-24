08a6721ab070498e54df62feb7f5280ac8d1b3b4
Richard Downer <richard.downer@xero.com>
2014-01-24 16:07:16 +1300

    Need to convert int to char

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,14 +1,28 @@
 package etl
 
+import (
+	"fmt"
+)
+
 // given map[int]array (old format)
 // return a map[string]int (new format)
 
-func Transform(map[int][]string) map[string]int {
-	return nil
+func Transform(input map[int][]string) map[string]int {
 
 	// for each record in the map input
 	// add the key and value to the new map
 	// old key becomes new value
 	// old value becomes new key
 
+	output := make(map[string]int)
+
+	for k, v := range input {
+		fmt.Println(k)
+		for i, s := range v {
+			fmt.Println(s[i])
+		}
+		fmt.Println("")
+	}
+
+	return output
 }
