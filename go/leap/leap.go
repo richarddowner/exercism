bb7f848803a874c88f46d1cf7580f985319e9209
Richard Downer <richard.downer@xero.com>
2014-01-28 09:27:51 +1300

    Removed repetition of %100, added back original comment

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,7 +1,8 @@
 package leap
 
-// if year is divisible by 4 and not 100 then is leap year
-// if year is divisible by 4, 100 and 400 then is leap year
+// If year is divisible by 400 then leap year
+// else if year is divisible by 100 then not leap year
+// else if year is divisible by 4 then leap year
 func IsLeapYear(year int) bool {
-	return year%4 == 0 && (year%100 != 0 || (year%100 == 0 && year%400 == 0))
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
