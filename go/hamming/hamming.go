bdd641edf3f5ed8dc91ef42ea2d7dc9098c84461
Richard Downer <richard.downer@xero.com>
2014-01-28 08:48:49 +1300

    Changed the way the foor loop works
    Included the conditional check in the for loop

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,18 +1,12 @@
 package hamming
 
-import (
-	"math"
-)
-
 func Distance(strandA string, strandB string) int {
 
 	distance := 0
 
-	for i := 0; i < int(math.Max(float64(len(strandA)), float64(len(strandB)))); i++ {
-		if (len(strandA)) > i && (len(strandB)) > i {
-			if strandA[i] != strandB[i] {
-				distance = distance + 1
-			}
+	for i := 0; len(strandA) > i && len(strandB) > i; i++ {
+		if strandA[i] != strandB[i] {
+			distance++
 		}
 	}
 	return distance
