bc7a08e9faae5e8165018652d689981addca0522
Richard Downer <richard.downer@xero.com>
2014-01-28 14:02:22 +1300

    Two sorted anagrams will be the same word

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -4,41 +4,10 @@ import (
 	"fmt"
 )
 
+// two sorted anagrams will always be the same word
 func Detect(subject string, candidates []string) []string {
 
 	var anagrams = make([]string, 0)
 
-	// for each candidate
-	for _, candidate := range candidates {
-
-		// check for same length
-		if len(subject) == len(candidate) {
-
-			var matches = 0
-			// for each char in subject
-			for _, subjectChar := range subject {
-				// check each char of candidate
-				for _, candidateChar := range candidate {
-
-					if subjectChar == candidateChar {
-						matches++
-					}
-				}
-			}
-			if matches == len(subject) {
-				// add candidate to the array
-				fmt.Println("anagram: ", subject, candidate)
-				anagrams = append(anagrams, candidate)
-			}
-
-		}
-
-	}
-
 	return anagrams
 }
-
-// for each candidate
-// loop over subject and candidate checking if characters match
-// if no match on one of the characters break
-// else add the candidate to the anagram list
