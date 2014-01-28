e299133ceda8a8e0a4e535c94c442b16a7e69926
Richard Downer <richard.downer@xero.com>
2014-01-28 13:47:28 +1300

    Multiple anagrams failing

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -8,19 +8,27 @@ func Detect(subject string, candidates []string) []string {
 
 	var anagrams = make([]string, 0)
 
+	// for each candidate
 	for _, candidate := range candidates {
 
 		// check for same length
 		if len(subject) == len(candidate) {
 
 			var matches = 0
-			for i, char := range subject {
-				if char == candidate[i] {
-					matches++
+			// for each char in subject
+			for _, subjectChar := range subject {
+				// check each char of candidate
+				for _, candidateChar := range candidate {
+
+					if subjectChar == candidateChar {
+						matches++
+					}
 				}
 			}
 			if matches == len(subject) {
 				// add candidate to the array
+				fmt.Println("anagram: ", subject, candidate)
+				anagrams = append(anagrams, candidate)
 			}
 
 		}
