037bf4b4281aad0ecb815019ac375b5c0a1952d6
Richard Downer <richard.downer@xero.com>
2014-01-28 14:37:43 +1300

    Anagram solved

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,39 +1,24 @@
 package anagram
 
 import (
-	"fmt"
 	"sort"
+	"strings"
 )
 
-// type ByLength []string
 type Rune []rune
 
-// func (s ByLength) Len() int {
-// 	return len(s)
-// }
 func (r Rune) Len() int {
 	return len(r)
 }
 
-// func (s ByLength) Swap(i, j int) {
-// 	s[i], s[j] = s[j], s[i]
-// }
 func (r Rune) Swap(i, j int) {
 	r[i], r[j] = r[j], r[i]
 }
 
-// func (s ByLength) Less(i, j int) bool {
-// 	return len(s[i]) < len(s[j])
-// }
 func (r Rune) Less(i, j int) bool {
 	return r[i] < r[j]
 }
 
-// func main() {
-// 	fruits := []string{"peach", "banana", "kiwi"}
-// 	sort.Sort(ByLength(fruits))
-// 	fmt.Println(fruits)
-// }
 func stringSort(s string) string {
 	runes := []rune(s)
 	sort.Sort(Rune(runes))
@@ -45,7 +30,24 @@ func Detect(subject string, candidates []string) []string {
 
 	var anagrams = make([]string, 0)
 
-	fmt.Println(subject, stringSort(subject))
+	// sort subject
+	sortedSubject := stringSort(strings.ToLower(subject))
 
+	// for each candidate
+	for _, candidate := range candidates {
+
+		candidate = strings.ToLower(candidate)
+
+		if subject == candidate {
+			continue
+		}
+
+		// sort the candidate
+		sortedCandidate := stringSort(candidate)
+
+		if sortedCandidate == sortedSubject {
+			anagrams = append(anagrams, candidate)
+		}
+	}
 	return anagrams
 }
