354ed44db8adb88d3d14d5914a29a431955fc797
Richard Downer <richard.downer@xero.com>
2014-01-31 09:48:09 +1300

    hamming changes

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,10 +1,15 @@
+/*
+Package hamming implements Distance;
+a simple function for calculating the
+hamming distance between two DNA strands.
+*/
 package hamming
 
-func Distance(strandA string, strandB string) (distance int) {
+func Distance(strandA string, strandB string) (hammingDistance int) {
 	for i := 0; len(strandA) > i && len(strandB) > i; i++ {
 		if strandA[i] != strandB[i] {
-			distance++
+			hammingDistance++
 		}
 	}
-	return distance
+	return
 }
