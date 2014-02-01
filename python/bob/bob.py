e43cd228396c0a7847c7590c71f810e8291526fa
Richard Downer <richarddowna@gmail.com>
2014-02-01 14:04:15 +1300

    Working on bob.py

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -3,11 +3,11 @@ class Bob:
 		# if sentence is empty or whitespace
 		if not sentence:
 			return 'Fine. Be that way!'
-		# if sentence ends with ?
-		if sentence[len(sentence)-1] == '?':
-			return 'Sure.'
 		# if sentence is all uppercase
 		if sentence == sentence.upper():
 			return 'Woah, chill out!'
+		# if sentence ends with ?
+		if sentence[len(sentence)-1] == '?':
+			return 'Sure.'
 		# else return default
 		return 'Whatever.'
