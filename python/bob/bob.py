7e0a72df7174daf532f62ff57235f240d062f667
Richard Downer <richarddowna@gmail.com>
2014-02-01 22:34:25 +1300

    Bob tests passing

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,10 +1,14 @@
+'''
+Bob listens to a sentence
+and replies accordingly.
+'''
 class Bob:
 	def hey(self, sentence):
 		# if sentence is empty or whitespace
 		if not sentence.strip():
 			return 'Fine. Be that way!'
-		# if sentence is all uppercase
-		if sentence == sentence.upper():
+		# if sentence is all uppercase and contains at least one uppercase letter
+		if sentence == sentence.upper() and any(x.isupper() for x in sentence):
 			return 'Woah, chill out!'
 		# if sentence ends with ?
 		if sentence[len(sentence)-1] == '?':
