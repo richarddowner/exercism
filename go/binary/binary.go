8c3b739c99bdc57ed527efae1a8829f9bf08e156
Richard Downer <richard.downer@xero.com>
2014-02-04 11:05:21 +1300

    Completed binary.go

--- go/binary/binary.go
+++ go/binary/binary.go
@@ -1,22 +1,22 @@
 package binary
 
 import (
-	"fmt"
 	"math"
 	"unicode/utf8"
 )
 
 func ToDecimal(binary string) (decimal int) {
-
-	// loop over string backwards
-
-	for i := 0; i < utf8.RuneCountInString(binary); i++ {
-		if binary[i] == '1' {
-			fmt.Println("its a 1")
-			decimal = decimal + math.Pow(float64(2), float64(i))
-			fmt.Println("decimal: ", decimal)
+	exponent := 0
+	for bit := utf8.RuneCountInString(binary) - 1; bit >= 0; bit-- {
+		if binary[bit] != '1' && binary[bit] != '0' {
+			// not a binary number
+			return 0
 		}
+		if binary[bit] == '1' {
+			// decimal += 2^exponent
+			decimal += int(math.Pow(float64(2), float64(exponent)))
+		}
+		exponent++
 	}
-
 	return
 }
