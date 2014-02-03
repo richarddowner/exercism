551826aa5beb1498dc912464364d4d01b789dce9
Richard Downer <richard.downer@xero.com>
2014-02-04 11:15:20 +1300

    Improved binary.go

--- go/binary/binary.go
+++ go/binary/binary.go
@@ -8,13 +8,12 @@ import (
 func ToDecimal(binary string) (decimal int) {
 	exponent := 0
 	for bit := utf8.RuneCountInString(binary) - 1; bit >= 0; bit-- {
-		if binary[bit] != '1' && binary[bit] != '0' {
-			// not a binary number
-			return 0
-		}
 		if binary[bit] == '1' {
 			// decimal += 2^exponent
 			decimal += int(math.Pow(float64(2), float64(exponent)))
+		} else if binary[bit] != '0' {
+			// not a binary number
+			return 0
 		}
 		exponent++
 	}
