5f5ef712b7d02cdae766fc7a94ecfcd8804df71f
Richard Downer <richard.downer@xero.com>
2014-02-07 08:17:01 +1300

    Tests passing bob.go

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,3 +1,9 @@
+/*
+Package bob implements Hey;
+a function that allows Alice to
+talk to Bob and obtain his very
+limited responses.
+*/
 package bob
 
 import (
@@ -5,14 +11,14 @@ import (
 )
 
 func Hey(words string) string {
-
+	if strings.Trim(words, " ") == "" {
+		return "Fine. Be that way!"
+	}
 	if words == strings.ToUpper(words) && strings.ToLower(words) != strings.ToUpper(words) {
 		return "Woah, chill out!"
 	}
-
 	if strings.HasSuffix(words, "?") {
 		return "Sure."
 	}
-
 	return "Whatever."
 }
