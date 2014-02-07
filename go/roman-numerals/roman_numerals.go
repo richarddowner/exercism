d2e70fe5b0a0b1ab00628eca2edb7cb97186df62
Richard <richarddowna@gmail.com>
2014-02-07 23:12:59 +1300

    Tidy up roman_numerals.go

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -5,39 +5,37 @@ import (
 	"strconv"
 )
 
-func ToRomanNumeral(arabic int) (returnRoman string) {
+func ToRomanNumeral(arabic int) (roman string) {
 
 	romanNumerals := [][]string{{"I", "V"}, {"X", "L"}, {"C", "D"}, {"M", ""}}
-	roman := make([]string, 0)
+	buildString := make([]string, 0)
 
-	// arabic = 543 // remove me
 	decimal := strconv.Itoa(arabic)
 
 	th := len(decimal) - 1
 	for i := 0; i < len(decimal); i++ {
 		if decimal[i] == '4' {
-			roman = append(roman, romanNumerals[th][0]+romanNumerals[th][1])
+			buildString = append(buildString, romanNumerals[th][0]+romanNumerals[th][1])
 		} else if decimal[i] == '5' {
-			roman = append(roman, romanNumerals[th][1])
+			buildString = append(buildString, romanNumerals[th][1])
 		} else if decimal[i] == '9' {
-			roman = append(roman, romanNumerals[th][0]+romanNumerals[th+1][0])
+			buildString = append(buildString, romanNumerals[th][0]+romanNumerals[th+1][0])
 		} else if decimal[i] < '5' {
 			num := int(decimal[i]) - 48
 			for j := 0; j < num; j++ {
-				roman = append(roman, romanNumerals[th][0])
+				buildString = append(buildString, romanNumerals[th][0])
 			}
 		} else if decimal[i] > '5' {
-			roman = append(roman, romanNumerals[th][1])
+			buildString = append(buildString, romanNumerals[th][1])
 			num := int(decimal[i]) - 48 - 5
 			for j := 0; j < num; j++ {
-				roman = append(roman, romanNumerals[th][0])
+				buildString = append(buildString, romanNumerals[th][0])
 			}
 		}
 		th--
 	}
-	// convert []string to string and return
-	for _, s := range roman {
-		returnRoman += s
+	for _, char := range buildString {
+		roman += char
 	}
-	return returnRoman
+	return
 }
