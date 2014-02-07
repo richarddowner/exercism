979857fb95063b068d93171ff41841d2dde5e9d8
Richard <richarddowna@gmail.com>
2014-02-07 22:38:56 +1300

    Working on roman_numerals.go

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -9,7 +9,7 @@ func ToRomanNumeral(arabic int) string {
 
 	roman := make([]string, 0)
 
-	arabic = 549
+	arabic = 6
 
 	romanNumerals := [][]string{{"I", "V"}, {"X", "L"}, {"C", "D"}, {"M", ""}}
 
@@ -21,21 +21,24 @@ func ToRomanNumeral(arabic int) string {
 	for i := 0; i < len(decimal); i++ {
 		if decimal[i] == '4' {
 			roman = append(roman, romanNumerals[th][0]+romanNumerals[th][1])
-		}
-		if decimal[i] == '5' {
+		} else if decimal[i] == '5' {
 			roman = append(roman, romanNumerals[th][1])
-		}
-		if decimal[i] == '9' {
+		} else if decimal[i] == '9' {
 			roman = append(roman, romanNumerals[th][0]+romanNumerals[th+1][0])
-		}
-		if decimal[i] < '5' {
-			// convert to int
-			// loop over adding romanNumerals[th][0] each time
-		}
-		if decimal[i] > '5' {
+		} else if decimal[i] < '5' {
+			num := int(decimal[i]) - 48
+			for j := 0; j < num; j++ {
+				roman = append(roman, romanNumerals[th][0])
+			}
+		} else if decimal[i] > '5' {
 			// add romanNumerals[th][1]
+			roman = append(roman, romanNumerals[th][1])
 			// convert to int
+			num := int(decimal[i]) - 48
 			// loop over adding romanNumerals[th][0] each time
+			for j := 0; j < num; j++ {
+				roman = append(roman, romanNumerals[th][0])
+			}
 		}
 		th--
 	}
@@ -43,49 +46,3 @@ func ToRomanNumeral(arabic int) string {
 	fmt.Println("roman = ", roman)
 	return "I"
 }
-
-// cases
-
-// x = 0
-// x = 1 - 3
-// x = 4
-// x = 5
-// x = 6 - 8
-// x = 9
-
-// x = 0
-// x = 4
-// x = 5
-// x = 9
-// x < 5 && x % 5 = r
-// x > 5 && x % 5 = r
-
-// 48
-// tenths
-// 4 % 5 = 4 ~ if 4: take (X L)
-// ones
-// 8 % 5 = 3 ~ (V I I I)
-
-// 59
-// tenths
-// 5 % 5 = 0 ~ if 0: take (L)
-// ones
-// 9 % 5 = 4 ~ (I X)
-
-// yes
-// 141
-// hundreths
-// 1 % 5 = 1 (C)
-// tenths
-// 4 % 5 = 4 ~ if 4: take (X L)
-// ones
-// 1 % 5 = 1 (I)
-
-// no
-// 575
-// hundreths
-// 5 % 5 = 0 ~ if 0: take (D)
-// tenths
-// 7 % 5 = 2 ~ (X X)
-// ones
-// 5 % 5 = 0 ~ if 0: take (V)
