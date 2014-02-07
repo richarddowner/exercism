931ee7e35235fb9faff397a563b25a82e64dbe29
Richard Downer <richard.downer@xero.com>
2014-02-07 16:24:57 +1300

    Improved bob.py

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -7,8 +7,8 @@ class Bob:
 		# if sentence is empty or whitespace
 		if not sentence.strip():
 			return 'Fine. Be that way!'
-		# if sentence is all uppercase and contains at least one uppercase letter
-		if sentence == sentence.upper() and any(x.isupper() for x in sentence):
+		# if sentence is all uppercase
+		if sentence == sentence.upper() and sentence.upper() != sentence.lower():
 			return 'Woah, chill out!'
 		# if sentence ends with ?
 		if sentence[len(sentence)-1] == '?':
