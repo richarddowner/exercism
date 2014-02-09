71e47164de1031140793cd15fcb33e52d50bc5b1
Richard Downer <richard.downer@xero.com>
2014-02-10 09:02:46 +1300

    Working on a different way of doing atbash_cipher.go

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,7 +1,8 @@
 package atbash
 
 import (
-	"regexp"
+	// "regexp"
+	// "fmt"
 	"strings"
 )
 
@@ -10,21 +11,33 @@ func Atbash(plaintext string) (ciphertext string) {
 	cipherkey := "abcdefghijklmnopqrstuvwxyz0123456789"
 
 	// remove punctuation - there must be a better way, this is horrible
-	tmpPlainText := ""
-	tmp := regexp.MustCompile("([a-zA-Z0-9])+").FindAllString(plaintext, -1)
-	for _, char := range tmp {
-		tmpPlainText += char
-	}
-	plaintext = tmpPlainText
+	// tmpPlainText := ""
+	// tmp := regexp.MustCompile("([a-zA-Z0-9])+").FindAllString(plaintext, -1)
+	// for _, char := range tmp {
+	// 	tmpPlainText += char
+	// }
+	// plaintext = tmpPlainText
 
 	// convert to lower and remove spaces
-	plaintext = strings.Replace(strings.ToLower(plaintext), " ", "", -1)
+
+	plaintext = strings.ToLower(plaintext)
+	plaintext = strings.Replace(plaintext, " ", "", -1)
 
 	for i := 0; i < len(plaintext); i++ {
+
+		// add a space every 5 chars
 		if i != 0 && i%5 == 0 {
 			ciphertext += " "
 		}
-		ciphertext += string(cipherkey[strings.Index(cipher, string(plaintext[i]))])
+
+		character := string(plaintext[i])
+		index := strings.Index(cipher, character)
+
+		if index >= 0 {
+			ciphertext += string(cipherkey[index])
+		}
+
 	}
-	return
+
+	return strings.TrimRight(ciphertext, " ")
 }
