c374d80ae0ef531aa0a3f8310a91b190e896d444
Richard Downer <richard.downer@xero.com>
2014-02-12 08:14:22 +1300

    Started bottle.go

---
+++ go/bottles/bottle.go
@@ -0,0 +1,30 @@
+package bottles
+
+import (
+	"strconv"
+)
+
+// number > 1: {8} {bottles} of beer on the wall, {8} {bottles} of beer.\nTake one down and pass it around, {7} bottles of beer on the wall.\n
+// number = 1: {1} {bottle} of beer on the wall, {1} {bottle} of beer.\nTake it down and pass it around, {no more} bottles of beer on the wall.\n
+// number = 0: {No more} {bottles} of beer on the wall, {no more} {bottles} of beer.\nGo to the store and buy some more, {99} bottles of beer on the wall.\n
+
+const words = "{n} {bottles} of beer on the wall, {n} {bottles} of beer.\n{task}, {n-1} bottles of beer on the wall.\n"
+
+func Verse(number int) string {
+	if number > 1 {
+		n := strconv.Itoa(number)
+		bottles := "bottles"
+		task := "Take one down and pass it around"
+	}
+	if number == 1 {
+		n := strconv.Itoa(number)
+		bottles := "bottle"
+		task := "Take it down and pass it around"
+	}
+	if number == 0 {
+		n := "no more"
+		bottles := "bottles"
+		task := "Go to the store and buy some more"
+	}
+	return (n + bottles + " of beer on the wall, " + n + bottles + "of beer.\n")
+}
