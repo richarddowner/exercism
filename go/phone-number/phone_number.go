43ac7dc3ecce05d854ddd302cb37a66a3cb31ada
Richard Downer <richard.downer@xero.com>
2014-03-21 07:59:51 +1300

    Changes to phone_number

--- go/phone-number/phone_number.go
+++ go/phone-number/phone_number.go
@@ -5,34 +5,38 @@ import (
 	"strings"
 )
 
-func FormatPhoneNumber(input string) string {
-	output := strings.Join(regexp.MustCompile("([0-9])+").FindAllString(input, -1), "")
-	invalidNumber := "0000000000"
-	if len(output) < 10 {
-		return invalidNumber
-	}
-	if len(output) == 11 && output[0] == '1' {
-		output = output[1:]
-	}
-	if len(output) == 11 && output[0] != '1' {
-		return invalidNumber
-	}
-	if len(output) > 11 {
-		return invalidNumber
-	}
-	return output
-}
+const invalidNumber string = "0000000000"
+
+var numbersRegex *regexp.Regexp = regexp.MustCompile("([0-9])+")
 
 func Number(input string) string {
-	return FormatPhoneNumber(input)
+	return formatPhoneNumber(input)
 }
 
 func AreaCode(input string) string {
-	output := FormatPhoneNumber(input)
+	output := formatPhoneNumber(input)
 	return output[0:3]
 }
 
 func Format(input string) string {
-	output := FormatPhoneNumber(input)
+	output := formatPhoneNumber(input)
 	return "(" + AreaCode(output) + ") " + output[3:6] + "-" + output[6:10]
+}
+
+func formatPhoneNumber(input string) string {
+	output := strings.Join(numbersRegex.FindAllString(input, -1), "")
+	length := len(output)
+	if length < 10 {
+		return invalidNumber
+	}
+	if length == 11 && output[0] == '1' {
+		output = output[1:]
+	}
+	if length == 11 && output[0] != '1' {
+		return invalidNumber
+	}
+	if length > 11 {
+		return invalidNumber
+	}
+	return output
 }
