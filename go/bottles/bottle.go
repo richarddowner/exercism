0096c0ae12468940328d0b6556a0dd6788f4fedf
Richard Downer <richard.downer@xero.com>
2014-05-06 14:34:33 +1200

    Try something new

--- go/bottles/bottle.go
+++ go/bottles/bottle.go
@@ -1,9 +1,9 @@
 package bottles
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
+// "fmt"
+// "strconv"
+// "strings"
 )
 
 // number > 1: {8} {bottles} of beer on the wall, {8} {bottles} of beer.\nTake one down and pass it around, {7} bottles of beer on the wall.\n
@@ -12,55 +12,19 @@ import (
 
 // const words = "{n} {bottles} of beer on the wall, {n} {bottles} of beer.\n{task}, {n-1} bottles of beer on the wall.\n"
 
+var bottles int = 99
+
 func Verse(number int) string {
 
-	n := ""
-	bottles := ""
-	task := ""
-
 	if number > 1 {
-		n = strconv.Itoa(number)
-		bottles = "bottles"
-		task = "Take one down and pass it around,"
+		// bottles--
+		// Verse(bottles)
 	}
 	if number == 1 {
-		n = strconv.Itoa(number)
-		bottles = "bottle"
-		task = "Take it down and pass it around,"
+		// bottles--
+		// Verse(bottles)
+	} else {
+		// bottles--
+		// Verse(bottles)
 	}
-	if number == 0 {
-		n = "no more"
-		bottles = "bottles"
-		task = "Go to the store and buy some more,"
-	}
-
-	words := n + " " + bottles + " of beer on the wall, " + n + " " + bottles + " of beer.\n" + task + " "
-
-	number = number - 1
-
-	if number > 1 {
-		n = strconv.Itoa(number)
-		bottles = "bottles"
-		task = "Take one down and pass it around,"
-	}
-	if number == 1 {
-		n = strconv.Itoa(number)
-		bottles = "bottle"
-		task = "Take it down and pass it around,"
-	}
-	if number == 0 {
-		n = "no more"
-		bottles = "bottles"
-		task = "Go to the store and buy some more,"
-	}
-
-	words = words + n + " " + bottles + " of beer on the wall.\n"
-
-	return capitalize(words)
-}
-
-func capitalize(words string) (output string) {
-	output = strings.ToUpper(string(words[0]))
-	output = output + words[1:]
-	return
 }
