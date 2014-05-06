051040f4630fdc5a62a507dd40d55ed1b1e1c5f2
Richard Downer <richard.downer@xero.com>
2014-05-06 14:53:22 +1200

    Added return variable

--- go/bottles/bottle.go
+++ go/bottles/bottle.go
@@ -1,8 +1,9 @@
 package bottles
 
 import (
-// "fmt"
-// "strconv"
+	//"fmt"
+	"strconv"
+
 // "strings"
 )
 
@@ -14,21 +15,29 @@ import (
 
 var bottles int = 99
 
-func Verse(number int) string {
+func Verse(number int) (output string) {
 
 	if number > 1 {
-		fmt.Printf("%d bottles of beer on the wall, %d bottles of beer.\n", bottles, bottles)
+		// fmt.Printf("%d bottles of beer on the wall, %d bottles of beer.\n", bottles, bottles)
+		output += strconv.Itoa(bottles) + " bottles of beer on the wall, " + strconv.Itoa(bottles) + " bottles of beer.\n"
 		bottles--
-		fmt.Println("Take one down and pass it around, %d bottles of beer on the wall.\n", bottles)
+		//fmt.Printf("Take one down and pass it around, %d bottles of beer on the wall.\n", bottles)
+		output += "Take one down and pass it around, " + strconv.Itoa(bottles) + " bottles of beer on the wall.\n"
 		Verse(bottles)
 	}
 	if number == 1 {
-		fmt.Println("1 bottle of beer on the wall, 1 bottle of beer.")
-		fmt.Println("Take one down and pass it around, no more bottles of beer on the wall.\n")
+		// fmt.Println("1 bottle of beer on the wall, 1 bottle of beer.")
+		output += "1 bottle of beer on the wall, 1 bottle of beer.\n"
+		// fmt.Println("Take one down and pass it around, no more bottles of beer on the wall.\n")
+		output += "Take one down and pass it around, no more bottles of beer on the wall.\n"
+		bottles--
 		Verse(bottles)
 	} else {
-		fmt.Println("No more bottles of beer on the wall, no more bottles of beer.")
-		fmt.Println("Go to the store and buy some more, 99 bottles of beer on the wall.")
+		// fmt.Println("No more bottles of beer on the wall, no more bottles of beer.")
+		output += "No more bottles of beer on the wall, no more bottles of beer.\n"
+		// fmt.Println("Go to the store and buy some more, 99 bottles of beer on the wall.")
+		output += "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
 		return
 	}
+	return
 }
