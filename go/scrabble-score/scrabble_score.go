252ccdd0f1cfa06ca0d72bb66c4cb11313913968
Richard Downer <richard.downer@xero.com>
2014-05-08 11:14:24 +1200

    Working on scrabble score

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,6 +1,11 @@
 package scrabble_score
 
-var b = [][]string{
+import (
+	"strconv"
+	"strings"
+)
+
+var b = [7][7]string{
 	{
 		"AEIOULNRST",
 		"DG",
@@ -15,6 +20,14 @@ var b = [][]string{
 	},
 }
 
-func Score(word string) int {
-
+func Score(word string) (score int) {
+	for i := 0; i < len(word); i++ {
+		for j := 0; j < len(b); j++ {
+			if strings.ToLower(word[i]) == strings.ToLower(b[0][j]) {
+				var point, _ = strconv.Atoi(b[1][j])
+				score += point
+			}
+		}
+	}
+	return
 }
