c559e620419fa5b81d35eb9fcf1bb6c16e812606
Richard Downer <richard.downer@xero.com>
2014-05-07 14:08:09 +1200

    Setup scrabble data structure

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,20 @@
+package scrabble_score
+
+var b = [][]string{
+	{
+		"AEIOULNRST",
+		"DG",
+		"BCMP",
+		"FHVWY",
+		"K",
+		"JX",
+		"QZ",
+	},
+	{
+		"1", "2", "3", "4", "5", "8", "10",
+	},
+}
+
+func Score(word string) int {
+
+}
