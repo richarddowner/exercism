cceb8c614093b30f638be1f0e978dc377706b4be
Richard Downer <richard.downer@xero.com>
2014-05-07 13:12:07 +1200

    float64(64) is overflowed

---
+++ go/grains/grains.go
@@ -0,0 +1,13 @@
+package grains
+
+import (
+	"math"
+)
+
+func Square(number int) uint64 {
+	return uint64(math.Pow(2, float64(number-1)))
+}
+
+func Total() uint64 {
+	return uint64(math.Pow(2, float64(64)))
+}
