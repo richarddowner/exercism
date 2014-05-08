d88ff18aaeb5c11102afd7a64b5af17914ece0a5
Richard Downer <richard.downer@xero.com>
2014-05-09 08:21:42 +1200

    Tidy up grains.go

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,9 +1,9 @@
 package grains
 
 func Square(number int) uint64 {
-	return uint64(1 << uint64(number-1))
+	return 1 << uint64(number-1)
 }
 
 func Total() uint64 {
-	return uint64(1<<63) + (uint64(1<<63) - 1)
+	return (1 << 64) - 1
 }
