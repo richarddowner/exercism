05e97c32206317636279dcc61ebecb059563d5d4
Richard Downer <richard.downer@xero.com>
2014-05-08 15:26:14 +1200

    Use logical shift instead of math.Pow

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,13 +1,9 @@
 package grains
 
-import (
-	"math"
-)
-
 func Square(number int) uint64 {
-	return uint64(math.Pow(2, float64(number-1)))
+	return uint64(1 << uint64(number-1))
 }
 
 func Total() uint64 {
-	return uint64(math.Pow(2, float64(63))) + uint64(math.Pow(2, float64(63))) - 1
+	return uint64(1<<63) + (uint64(1<<63) - 1)
 }
