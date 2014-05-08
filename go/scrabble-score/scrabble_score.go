84f28d65fec07f541f93790721340b3610a712fb
Richard Downer <richard.downer@xero.com>
2014-05-08 13:58:44 +1200

    working on scrabble

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,6 +1,7 @@
 package scrabble_score
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -23,7 +24,14 @@ var b = [7][7]string{
 func Score(word string) (score int) {
 	for i := 0; i < len(word); i++ {
 		for j := 0; j < len(b); j++ {
-			if strings.ToLower(word[i]) == strings.ToLower(b[0][j]) {
+
+			fmt.Println(strings.ToUpper(string(word[i])))
+			fmt.Println(b[0][j])
+			fmt.Println()
+
+			fmt.Println(strings.Contains(b[0][j], string(word[i])))
+
+			if strconv.Itoa(int(word[i])) == b[0][j] {
 				var point, _ = strconv.Atoi(b[1][j])
 				score += point
 			}
