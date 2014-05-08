40536c8f8d945144a74af8aff23030f9f7ce69ce
Richard Downer <richard.downer@xero.com>
2014-05-09 08:21:58 +1200

    Work on scrabble.go

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -2,7 +2,7 @@ package scrabble_score
 
 import (
 	"fmt"
-	"strconv"
+	// "strconv"
 	"strings"
 )
 
@@ -22,20 +22,32 @@ var b = [7][7]string{
 }
 
 func Score(word string) (score int) {
-	for i := 0; i < len(word); i++ {
-		for j := 0; j < len(b); j++ {
 
-			fmt.Println(strings.ToUpper(string(word[i])))
-			fmt.Println(b[0][j])
-			fmt.Println()
+	for i := 0; i < 7; i++ {
+		fmt.Println(word)
+		for j := 0; j < len(word); j++ {
+			fmt.Println(strings.ToUpper(string(word[j])))
+			fmt.Println(b[0][i])
+			fmt.Println(strings.Contains(b[0][i], string(word[j])))
 
-			fmt.Println(strings.Contains(b[0][j], string(word[i])))
-
-			if strconv.Itoa(int(word[i])) == b[0][j] {
-				var point, _ = strconv.Atoi(b[1][j])
-				score += point
-			}
 		}
+		fmt.Println()
 	}
+
+	// for i := 0; i < len(word); i++ {
+	// 	for j := 0; j < len(b); j++ {
+
+	// 		fmt.Println(strings.ToUpper(string(word[i])))
+	// 		fmt.Println(b[0][j])
+	// 		fmt.Println(strings.Contains(b[0][j], string(word[i])))
+
+	// 		fmt.Println()
+
+	// 		if strconv.Itoa(int(word[i])) == b[0][j] {
+	// 			var point, _ = strconv.Atoi(b[1][j])
+	// 			score += point
+	// 		}
+	// 	}
+	// }
 	return
 }
