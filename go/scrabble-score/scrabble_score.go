e0b3052edba5099dce232c6d087103efd3d7ee97
Richard Downer <richard.downer@xero.com>
2014-05-12 08:52:28 +1200

    scrabble improvements

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,31 +1,26 @@
 package scrabble_score
 
 import (
-	"strconv"
 	"strings"
 )
 
-var b = [7][7]string{
-	{
-		"AEIOULNRST",
-		"DG",
-		"BCMP",
-		"FHVWY",
-		"K",
-		"JX",
-		"QZ",
-	},
-	{
-		"1", "2", "3", "4", "5", "8", "10",
-	},
+var letters = []string{
+	"AEIOULNRST",
+	"DG",
+	"BCMP",
+	"FHVWY",
+	"K",
+	"JX",
+	"QZ",
 }
+
+var scores = []int{1, 2, 3, 4, 5, 8, 10}
 
 func Score(word string) (score int) {
 	for _, char := range word {
-		for j := 0; j < 7; j++ {
-			if strings.Contains(b[0][j], strings.ToUpper(string(char))) {
-				point, _ := strconv.Atoi(b[1][j])
-				score += point
+		for i := 0; i < 7; i++ {
+			if strings.Contains(letters[i], strings.ToUpper(string(char))) {
+				score += scores[i]
 				break
 			}
 		}
