5da0874c12715c2b5330fac945d99142b4fff4bf
Richard Downer <richard.downer@xero.com>
2014-05-20 09:01:17 +1200

    grains.go new documentation

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,19 +1,15 @@
-/*
-Package grains implements:
-
-Square; a function that takes an integer
-representing a square postion on a chessboard.
-Returns a grain count such that grains = 2^(number - 1).
-
-Total; a function that returns the total number
-of grains on a chess board, such that total grains = 2^(64-1)
-*/
+// Package grains implements functions to count the
+// number of grains on a chessboard.
 package grains
 
+// Square takes an integer representing a square postion on a chessboard.
+// Returns a grain count such that grains = 2^(number - 1).
 func Square(number int) uint64 {
 	return 1 << uint64(number-1)
 }
 
+// Total returns the total number of grains on a chess board,
+// such that total grains = 2^(64-1)
 func Total() uint64 {
 	return (1 << 64) - 1
 }
