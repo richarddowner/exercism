3c6a9897a602979d559e1b04c102108f162411a0
Richard Downer <richard.downer@xero.com>
2014-05-22 14:51:44 +1200

    Some improvements to phone_number.go

--- go/phone-number/phone_number.go
+++ go/phone-number/phone_number.go
@@ -10,23 +10,9 @@ const invalidNumber string = "0000000000"
 var numbersRegex *regexp.Regexp = regexp.MustCompile("([0-9])+")
 
 func Number(input string) string {
-	return formatPhoneNumber(input)
-}
-
-func AreaCode(input string) string {
-	output := formatPhoneNumber(input)
-	return output[0:3]
-}
-
-func Format(input string) string {
-	output := formatPhoneNumber(input)
-	return "(" + AreaCode(output) + ") " + output[3:6] + "-" + output[6:10]
-}
-
-func formatPhoneNumber(input string) string {
 	output := strings.Join(numbersRegex.FindAllString(input, -1), "")
 	length := len(output)
-	if length < 10 {
+	if length < 10 || length > 11 {
 		return invalidNumber
 	}
 	if length == 11 && output[0] == '1' {
@@ -35,8 +21,14 @@ func formatPhoneNumber(input string) string {
 	if length == 11 && output[0] != '1' {
 		return invalidNumber
 	}
-	if length > 11 {
-		return invalidNumber
-	}
 	return output
+}
+
+func AreaCode(input string) string {
+	return Number(input)[0:3]
+}
+
+func Format(input string) string {
+	output := Number(input)
+	return "(" + AreaCode(output) + ") " + output[3:6] + "-" + output[6:10]
 }
