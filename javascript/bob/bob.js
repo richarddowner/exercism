0e6af5929370718dd0a00574bd258652e82b02c1
Richard Downer <richard.downer@xero.com>
2014-06-03 08:10:03 +1200

    JS is confusing nowdays

---
+++ javascript/bob/bob.js
@@ -0,0 +1,7 @@
+// http://docs.busterjs.org/en/latest/modules/buster-test/spec/
+// http://www.phpied.com/3-ways-to-define-a-javascript-class/
+function Bob(type) {
+	this.hey = function(var words) {
+
+	};
+}
