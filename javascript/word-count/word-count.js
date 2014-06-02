e79b871dffb5108407ae726b16c6e2e75b9636f8
Richard Downer <richard.downer@xero.com>
2014-06-03 10:01:41 +1200

    Working out how to do objects in javascript

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,4 +1,10 @@
+// need to take a string "one two one"
+// return an object of { one:2, two:1 },
+// where key = word, and value = word count
 var words = function(input) {
-	return { word : 1 };
+	var objArray = {};
+	objArray["word"] = 1;
+	// objArray["now"] = 2;
+	return objArray;
 }
 module.exports = words;
