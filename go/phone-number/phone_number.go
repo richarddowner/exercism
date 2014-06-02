e18b2a42279c40a28490bc8f3a3b206dd5928f6a
Richard Downer <richard.downer@xero.com>
2014-06-03 07:57:36 +1200

    Making phone number.go better

--- go/phone-number/phone_number.go
+++ go/phone-number/phone_number.go
@@ -10,25 +10,30 @@ const INVALID_NUMBER = "0000000000"
 
 var numbersRegex *regexp.Regexp = regexp.MustCompile("([0-9])+")
 
-func Number(input string) string {
-	output := strings.Join(numbersRegex.FindAllString(input, -1), "")
-	length := len(output)
+// Number takes a phone number as a string of digits and validates the
+// digits as a phone number. Returns an invalid number or valid number.
+func Number(digits string) string {
+	validDigits := strings.Join(numbersRegex.FindAllString(digits, -1), "")
+	length := len(validDigits)
 	switch {
 	case length < 10:
 		return INVALID_NUMBER
 	case length == 10:
-		return output
-	case (length == 11 && output[0] == '1'):
-		return output[1:]
+		return validDigits
+	case (length == 11 && validDigits[0] == '1'):
+		return validDigits[1:]
 	}
 	return INVALID_NUMBER
 }
 
-func AreaCode(input string) string {
-	return Number(input)[0:3]
+// AreaCode takes a phone number as a string of digits and returns the areacode.
+func AreaCode(digits string) string {
+	return Number(digits)[0:3]
 }
 
-func Format(input string) string {
-	output := Number(input)
-	return fmt.Sprintf("(%s) %s-%s", output[0:3], output[3:6], output[6:10])
+// Format takes a phone number as a string of digits, processes the numbers
+// and returns them in a correct phone number format.
+func Format(digits string) string {
+	validDigits := Number(digits)
+	return fmt.Sprintf("(%s) %s-%s", validDigits[0:3], validDigits[3:6], validDigits[6:10])
 }
