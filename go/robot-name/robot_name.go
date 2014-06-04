d2123b20cfb99fe31f536cc51beacb9bd2cab51b
Richard Downer <richard.downer@xero.com>
2014-06-05 09:18:23 +1200

    rand.Seed is not working one after the other. time.Now() has not changed, program runs to fast.

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,13 +1,29 @@
 package robotname
 
+import (
+	"math/rand"
+	"strconv"
+	"time"
+)
+
 type Robot struct {
 	name string
 }
 
-func (r *Robot) Name() string {
-	return "Test"
+func (r *Robot) Name() (name string) {
+	name += string(randInt(65, 90))
+	name += string(randInt(65, 90))
+	name += strconv.Itoa(randInt(0, 9))
+	name += strconv.Itoa(randInt(0, 9))
+	name += strconv.Itoa(randInt(0, 9))
+	return name
 }
 
 func (r *Robot) Reset() {
 
+}
+
+func randInt(min int, max int) (num int) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return min + rand.Intn(max-min)
 }
