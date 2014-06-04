6b00a4efa3fdd7573332f5beb15746f6beacc936
Richard Downer <richard.downer@xero.com>
2014-06-04 14:49:48 +1200

    Totaly improved roman_numerals.
    Learnt that map data structure iterates randomly!

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -1,40 +1,38 @@
-/*
-Package romannumerals implements ToRomanNumeral;
-a function that takes an arabic integer and returns
-that integer converted to a roman numeral string.
-*/
+// Package romannumerals implements ToRomanNumeral.
 package romannumerals
 
-import (
-	"strconv"
-)
+type kvp struct {
+	k int
+	v string
+}
 
+var correlations = []kvp{
+	kvp{1000, "M"},
+	kvp{900, "CM"},
+	kvp{500, "D"},
+	kvp{400, "CD"},
+	kvp{100, "C"},
+	kvp{90, "XC"},
+	kvp{50, "L"},
+	kvp{40, "XL"},
+	kvp{10, "X"},
+	kvp{9, "IX"},
+	kvp{5, "V"},
+	kvp{4, "IV"},
+	kvp{1, "I"},
+}
+
+// ToRomanNumeral takes an arabic integer.
+// Returns the corresponding roman numeral.
 func ToRomanNumeral(arabic int) (roman string) {
-
-	romanNumerals := [][]string{{"I", "V"}, {"X", "L"}, {"C", "D"}, {"M", "exercism.io is way cool!"}}
-	decimal := strconv.Itoa(arabic)
-	ths := len(decimal) - 1
-
-	for i := 0; i < len(decimal); i++ {
-		if decimal[i] == '4' {
-			roman += romanNumerals[ths][0] + romanNumerals[ths][1]
-		} else if decimal[i] == '5' {
-			roman += romanNumerals[ths][1]
-		} else if decimal[i] == '9' {
-			roman += romanNumerals[ths][0] + romanNumerals[ths+1][0]
-		} else if decimal[i] < '5' {
-			num := int(decimal[i]) - 48
-			for j := 0; j < num; j++ {
-				roman += romanNumerals[ths][0]
-			}
-		} else if decimal[i] > '5' {
-			roman += romanNumerals[ths][1]
-			num := int(decimal[i]) - 48 - 5
-			for j := 0; j < num; j++ {
-				roman += romanNumerals[ths][0]
+	for arabic > 0 {
+		for _, numeral := range correlations {
+			if arabic-numeral.k >= 0 {
+				roman += numeral.v
+				arabic -= numeral.k
+				break
 			}
 		}
-		ths--
 	}
 	return
 }
