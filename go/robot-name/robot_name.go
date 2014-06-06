ce4bc141b4f390df8af1294104ac935057db5d17
Richard Downer <richard.downer@xero.com>
2014-06-06 16:18:43 +1200

    working on robot

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,32 +1,36 @@
 package robotname
 
 import (
-	"fmt"
+	//"fmt"
 	"math/rand"
 	"strconv"
 	"time"
 )
+
+var identity string = ""
 
 type Robot struct {
 	name string
 }
 
 func (r *Robot) Name() (name string) {
-	rand.Seed(time.Now().UTC().UnixNano())
-	name += string(randInt(65, 90))
-	name += string(randInt(65, 90))
-	name += strconv.Itoa(randInt(0, 9))
-	name += strconv.Itoa(randInt(0, 9))
-	name += strconv.Itoa(randInt(0, 9))
-	return name
+	if identity == "" {
+		rand.Seed(time.Now().UTC().UnixNano()) // this needs to be lazy init
+		name += string(randInt(65, 90))
+		name += string(randInt(65, 90))
+		name += strconv.Itoa(randInt(0, 9))
+		name += strconv.Itoa(randInt(0, 9))
+		name += strconv.Itoa(randInt(0, 9))
+		identity = name
+	}
+	return identity
 }
 
 func (r *Robot) Reset() {
-
+	identity = ""
 }
 
 func randInt(min int, max int) (num int) {
 	num = rand.Intn(max-min) + min
-	fmt.Println(num)
 	return
 }
